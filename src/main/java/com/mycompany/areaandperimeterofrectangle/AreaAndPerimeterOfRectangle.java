1920f7eb5a029209494da856edf9c39bb7a3520a
24002110 <tumelomashabathaka@gmail.com>
2025-03-06 20:40:12 +0200

    First commit

---
+++ src/main/java/com/mycompany/areaandperimeterofrectangle/AreaAndPerimeterOfRectangle.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.areaandperimeterofrectangle;
+
+/**
+ *
+ * @author tumel
+ */
+public class AreaAndPerimeterOfRectangle {
+
+    public static void main(String[] args) {
+        double width=5.6;
+        double height=8.5;
+        
+        double perimeter=2*(height+width);
+        double area=width*height;
+        
+        System.out.println("The perimeter is  "+perimeter);
+        System.out.println("The area is "+area);
+    }
+}
